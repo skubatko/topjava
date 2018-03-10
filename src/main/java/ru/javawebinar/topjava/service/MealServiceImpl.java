3cab4831ed98feee35468bfe1cce9b950680114c
Sergey Kubatko <skubatko@gmail.com>
2018-03-11 00:28:50 +0300

    HW2: Spring annotations assigned for Meal

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,9 +1,16 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+@Service
 public class MealServiceImpl implements MealService {
 
-    private MealRepository repository;
+    @Autowired
+    private final MealRepository repository;
 
+    public MealServiceImpl( MealRepository repository ) {
+        this.repository = repository;
+    }
 }
--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -14,9 +14,9 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
     private final UserRepository repository;
 
-    @Autowired
     public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
     }
