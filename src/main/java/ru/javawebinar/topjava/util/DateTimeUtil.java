b1b5b188de1fcc5666d50804f4b2fb85bc3a1be2
Sergey Kubatko <skubatko@gmail.com>
2018-03-14 00:45:42 +0300

    HW2_v_2.0: generic implemented into DateTimeUtil

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -5,13 +5,13 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm" );
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
+    public static <T extends LocalDateTime> boolean isBetween( T lt, T startTime, T endTime ) {
+        return lt.compareTo( startTime ) >= 0 && lt.compareTo( endTime ) <= 0;
     }
 
-    public static String toString(LocalDateTime ldt) {
-        return ldt == null ? "" : ldt.format(DATE_TIME_FORMATTER);
+    public static String toString( LocalDateTime ldt ) {
+        return ldt == null ? "" : ldt.format( DATE_TIME_FORMATTER );
     }
 }
