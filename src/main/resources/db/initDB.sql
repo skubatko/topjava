fc147def18b198bde435e7af8f28707865fba949
Sergey Kubatko <skubatko@gmail.com>
2018-03-21 13:12:01 +0300

    HW3: main + optional completed

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -34,3 +34,4 @@ CREATE TABLE meals
   user_id          INTEGER                 NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE UNIQUE INDEX meals_unique_date_time_idx ON meals (date_time);
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -4,12 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
@@ -47,6 +50,11 @@ public class MealServiceTest {
         List<Meal> meals = new ArrayList<>( ADMIN_MEALS );
         meals.add( newMeal );
         assertMatch( service.getAll( ADMIN_ID ), meals );
+    }
+
+    @Test( expected = DataAccessException.class )
+    public void duplicateDateTimeCreate() throws Exception {
+        service.create( new Meal( null, LocalDateTime.of( 2015, Month.MAY, 30, 20, 0 ), "Dublicated", 170 ), USER_ID );
     }
 
     @Test
